9cc5a9828f24b75d821bc7a3cae277a2c4e8bb30
Avishka Indula <avishkainduladev@gmail.com>
2022-12-17 08:22:11 +0530

    Working with "props" & Dynamic Content
    => adding text attributes to components in App.js
    => adding props parameter to Todo function in Todo.js
    => outputting text attributes inside <h1> of Todo function
    *  as dynamic JavaScript expressions.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learn React" />
       {/* This is a custom element (a custom component) */}
       {/* In order to differentiate these custom elements with */}
       {/* Built in html elements, we need to create this in */}
       {/* capital letters. */}
-      <Todo />
-      <Todo />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,8 @@
-function Todo() {
+function Todo(props) {
   // Function name should start with a capital letter.
   return (
     <div className="card">
-      <h2>TITLE</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
         <button className="btn">Delete</button>
       </div>
