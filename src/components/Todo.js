ff23a539dc0b8c5d942243cdf68c46e725ee4bc6
Avishka Indula <avishkainduladev@gmail.com>
2022-12-18 16:21:40 +0530

    Handling Events
    => adding onClick attribute to delete button
    => creating deleteHandler() function
    => assign deleteHandler function as the value of the onClick attribute.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,23 @@
 function Todo(props) {
   // Function name should start with a capital letter.
+
+  function deleteHandler() {
+    console.log("Clicked!");
+    console.log(props.text);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
+      {/* This is how we access the values of attributes we */}
+      {/* pass to the component tags on App.js*/}
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          {/* We don't add () to deleteHandler here. */}
+          {/* Because we only wanna execute deleteHandler function */}
+          {/* when the button is clicked. */}
+          Delete
+        </button>
       </div>
     </div>
   );
